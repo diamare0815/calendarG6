3c6c0407ec4783a86e15caa6dcc57d4fe2d0e7f1
mnguyet <minhnguyet2022005@gmail.com>
2025-12-13 14:13:51 +0700

    full project

--- app/src/main/java/com/calendar/cute/database/entities/UserEntity.java
+++ app/src/main/java/com/calendar/cute/database/entities/UserEntity.java
@@ -1,4 +1,35 @@
 package com.calendar.cute.database.entities;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
 public class UserEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String name;
+
+    public UserEntity(String name) {
+        this.name = name;
+    }
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
