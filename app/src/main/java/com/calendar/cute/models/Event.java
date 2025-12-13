a55eaa9550da400d6ca3e6a0d45e681fce9aedb1
mnguyet <minhnguyet2022005@gmail.com>
2025-12-13 14:29:39 +0700

    full project

--- app/src/main/java/com/calendar/cute/models/Event.java
+++ app/src/main/java/com/calendar/cute/models/Event.java
@@ -1,28 +1,78 @@
 package com.calendar.cute.models;
 
-public class Event {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Event implements Serializable {
+
     private String title;
-    private String time;
+    private String timeStart;
+    private String timeEnd;
     private String date;
     private String color;
-    private boolean hasReminder;
 
-    public Event(String title, String time, String date, String color) {
+    private boolean hasReminder = false;
+    private int reminderMinutes = 0;
+
+    public Event(String title, String timeStart, String timeEnd, String date, String color){
         this.title = title;
-        this.time = time;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
         this.date = date;
         this.color = color;
-        this.hasReminder = false;
     }
 
+    // getter/setter
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-    public String getTime() { return time; }
-    public void setTime(String time) { this.time = time; }
+
+    public String getTimeStart() { return timeStart; }
+    public void setTimeStart(String timeStart) { this.timeStart = timeStart; }
+
+    public String getTimeEnd() { return timeEnd; }
+    public void setTimeEnd(String timeEnd) { this.timeEnd = timeEnd; }
+
     public String getDate() { return date; }
     public void setDate(String date) { this.date = date; }
+
     public String getColor() { return color; }
     public void setColor(String color) { this.color = color; }
+
     public boolean hasReminder() { return hasReminder; }
     public void setHasReminder(boolean hasReminder) { this.hasReminder = hasReminder; }
+
+    public int getReminderMinutes() { return reminderMinutes; }
+    public void setReminderMinutes(int reminderMinutes) { this.reminderMinutes = reminderMinutes; }
+
+    // Helpful overrides for stable identity (useful for alarm requestCode generation, comparisons...)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+        return Objects.equals(title, event.title) &&
+                Objects.equals(timeStart, event.timeStart) &&
+                Objects.equals(timeEnd, event.timeEnd) &&
+                Objects.equals(date, event.date) &&
+                Objects.equals(color, event.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, timeStart, timeEnd, date, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "title='" + title + '\'' +
+                ", timeStart='" + timeStart + '\'' +
+                ", timeEnd='" + timeEnd + '\'' +
+                ", date='" + date + '\'' +
+                ", color='" + color + '\'' +
+                ", hasReminder=" + hasReminder +
+                ", reminderMinutes=" + reminderMinutes +
+                '}';
+    }
 }
